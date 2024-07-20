bc2f7f2f3b252f591f779d907f93c01c17793e8f
Angel Smith <anngelsmiths@gmail.com>
2024-07-20 17:42:56 -0600

    new URL

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-1b893529-a888-4a73-927a-0e9f389b9e5d.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-b0942398-cc88-4f9b-a19b-76a4152f7c1b.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits"
