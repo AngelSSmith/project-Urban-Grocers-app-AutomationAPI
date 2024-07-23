4d15f31b2a2fbfa81b9f370592762c8002f3722c
Angel Smith <anngelsmiths@gmail.com>
2024-07-22 23:38:20 -0600

    confirmacion

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-b0942398-cc88-4f9b-a19b-76a4152f7c1b.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-8f0e20ee-f1f5-4792-a8a9-3e18ee39701a.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits"
